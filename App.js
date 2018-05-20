83d6df4f9c8b4962cba5840e2b2d4b9b997a541a
John <john@redstubble.com>
2018-05-20 15:21:08 +1200

    Update broken

--- App.js
+++ App.js
@@ -1,57 +1,87 @@
-// @flow
-import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
-import { createMaterialTopTabNavigator } from 'react-navigation';
+import React from "react";
+import { Button, Platform, SafeAreaView, StatusBar, StyleSheet, Text } from "react-native";
+import { createBottomTabNavigator } from "react-navigation";
+
+console.log(Object.keys(createBottomTabNavigator));
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ecf0f1",
+    justifyContent: "center",
+    alignItems: "center"
   },
+  paragraph: {
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#34495e"
+  }
 });
 
-function Trade() {
-  return (
-    <View style={styles.container}>
-      <Text>Trade</Text>
-    </View>
-  );
-}
+const isAndroid = Platform.OS === "android";
 
-function Quiz() {
-  return (
-    <View style={styles.container}>
-      <Text>Quiz</Text>
-    </View>
-  );
-}
-
-function Posts() {
-  return (
-    <View style={styles.container}>
-      <Text>Posts</Text>
-    </View>
-  );
-}
-
-const Tabs = createMaterialTopTabNavigator({
-  Trade: {
-    screen: Trade,
-  },
-  Quiz: {
-    screen: Quiz,
-  },
-  Posts: {
-    screen: Posts,
-  },
-});
-
-export default class App extends React.Component {
+class Trade extends React.Component {
   render() {
     return (
-      <Tabs style={{ paddingTop: 100, backgroundColor: '#000' }} />
+      <SafeAreaView style={[styles.container, { backgroundColor: "#6a51ae" }]}>
+        <StatusBar barStyle="light-content" backgroundColor="#6a51ae" />
+        <Text style={[styles.paragraph, { color: "#fff" }]}>Trade Screen</Text>
+      </SafeAreaView>
     );
   }
 }
+
+class Quiz extends React.Component {
+  render() {
+    return (
+      <SafeAreaView style={[styles.container, { backgroundColor: "#ecf0f1" }]}>
+        <Text style={styles.paragraph}>Quiz Screen</Text>
+        <Button
+          title="Trade screen"
+          onPress={() => this.props.navigation.navigate("Trade")}
+        />
+      </SafeAreaView>
+    );
+  }
+}
+
+class Post extends React.Component {
+  render() {
+    return (
+      <SafeAreaView style={[styles.container, { backgroundColor: "#008B7D" }]}>
+        <Text style={[styles.paragraph, { color: "#fff" }]}>Post Screen</Text>
+        <Button
+          title="Trade screen"
+          onPress={() => this.props.navigation.navigate("Trade")}
+          color={isAndroid ? "blue" : "#fff"}
+        />
+      </SafeAreaView>
+    );
+  }
+}
+
+export default createBottomTabNavigator(
+  {
+    Trade: {
+      screen: Trade
+    },
+    Quiz: {
+      screen: Quiz
+    },
+    Post: {
+      screen: Post
+    }
+  },
+  {
+    navigationOptions: ({navigation} = this.navigation) => ({
+      tabBarOnPress: ({ previousScene, scene, jumpToIndex }) => {
+        // TODO: This doesn't handle the initial render because the second screen renders last. What to do?
+        StatusBar.setBarStyle("light-content");
+        isAndroid && StatusBar.setBackgroundColor("#6a51ae");
+        // jumpToIndex(scene.index);
+      }
+    InitialRouteName: "Home",
+    headerMode: "none"
+  }
+);
