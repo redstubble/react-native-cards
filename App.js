ac39a68a544826b97acccce035e705215d376fe4
John <john@redstubble.com>
2018-05-23 11:30:46 +1200

    Bottom Navigation

--- App.js
+++ App.js
@@ -1,14 +1,9 @@
+/* eslint react/prop-types: 0 */
 import React from 'react';
-import {
-  Platform,
-  StyleSheet
-} from 'react-native';
-import { createBottomTabNavigator } from 'react-navigation';
-import Post from './Component/post';
-import Quiz from './Component/quiz';
-import Trade from './Component/trade';
-
-console.log(Object.keys(createBottomTabNavigator));
+import { Platform, StyleSheet, Text, Button } from 'react-native';
+import { SafeAreaView, createBottomTabNavigator } from 'react-navigation';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import PropTypes from 'prop-types';
 
 const styles = StyleSheet.create({
   container: {
@@ -26,17 +21,82 @@ const styles = StyleSheet.create({
 });
 
 const isAndroid = Platform.OS === 'android';
+const iconName = isAndroid ? 'md' : 'ios';
+
+const Post = ({ navigation }) => (
+  <SafeAreaView style={[styles.container, { backgroundColor: '#008B7D' }]}>
+    <Text style={[styles.paragraph, { color: '#fff' }]}>Post Screen</Text>
+    <Button
+      title="Trade screen"
+      onPress={() => navigation.navigate('Trade')}
+      color={isAndroid ? 'blue' : '#fff'}
+    />
+  </SafeAreaView>
+);
+
+const Quiz = ({ navigation }) => (
+  <SafeAreaView style={[styles.container, { backgroundColor: '#ecf0f1' }]}>
+    <Text style={styles.paragraph}>Quiz Screen</Text>
+    <Button title="Trade screen" onPress={() => navigation.navigate('Quiz')} />
+  </SafeAreaView>
+);
+
+const Trade = () => (
+  <SafeAreaView style={[styles.container, { backgroundColor: '#6a51ae' }]}>
+    <Text style={[styles.paragraph, { color: '#fff' }]}>Trade Screen</Text>
+  </SafeAreaView>
+);
 
 export default createBottomTabNavigator(
   {
     Trade: {
       screen: Trade,
+      path: '/',
+      navigationOptions: {
+        title: 'Entry Exit Positions',
+        tabBarLabel: 'Trade',
+        tabBarIcon: ({ tintColor, focused }) => (
+          <Ionicons
+            name={focused ? `${iconName}-analytics` : `${iconName}-trending-up`}
+            size={26}
+            style={{ color: tintColor }}
+          />
+        ),
+      },
     },
     Quiz: {
       screen: Quiz,
+      path: '/',
+      navigationOptions: {
+        title: 'Questions',
+        tabBarLabel: 'Quiz',
+        tabBarIcon: ({ tintColor, focused }) => (
+          <Ionicons
+            name={
+              focused
+                ? `${iconName}-checkmark-circle`
+                : `${iconName}-checkmark-circle-outline`
+            }
+            size={26}
+            style={{ color: tintColor }}
+          />
+        ),
+      },
     },
     Post: {
       screen: Post,
+      path: '/',
+      navigationOptions: {
+        title: 'Posts',
+        tabBarLabel: 'Posts',
+        tabBarIcon: ({ tintColor, focused }) => (
+          <Ionicons
+            name={focused ? `${iconName}-list-box` : `${iconName}-list`}
+            size={26}
+            style={{ color: tintColor }}
+          />
+        ),
+      },
     },
   },
   {
@@ -46,9 +106,16 @@ export default createBottomTabNavigator(
         // isAndroid && StatusBar.setBackgroundColor('#6a51ae');
         // jumpToIndex(scene.index);
       }),
-      InitialRouteName: 'Home',
+      InitialRouteName: 'Trade',
       headerMode: 'none',
-      screenProps: { styles, isAndroid },
     },
-  }
+  },
 );
+
+Quiz.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+  }).isRequired,
+};
+Post.propTypes = Quiz.propTypes;
+Trade.propTypes = Quiz.propTypes;
